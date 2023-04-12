d7542c6a0e5ec8cb0d3e3c7292ab4734b90791ea
GeoReece000 <geofreytegeret@gmail.com>
2023-04-12 12:08:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Create a Set to store the complements of each element in the array
+  const complements = new Set();
+  
+  // Iterate over each element in the array
+  for (const num of array) {
+    // Check if the complement of the current element is in the Set
+    if (complements.has(target - num)) {
+      // If so, we have found a pair of numbers that add up to the target
+      return true;
+    }
+    
+    // Otherwise, add the current element's complement to the Set
+    complements.add(num);
+  }
+  
+  // If we iterate over the entire array without finding a pair that adds up to the target, return false
+  return false;
+
 }
 
 /* 
-  Write the Big O time complexity of your function here
+ O(n)
 */
 
 /* 
-  Add your pseudocode here
+  Create a Set to store the complements of each element in the array
+Iterate over each element in the array
+Check if the complement of the current element is in the Set
+If so, we have found a pair of numbers that add up to the target
+Otherwise, add the current element's complement to the Set
+If we iterate over the entire array without finding a pair that adds up to the target, return false
+
 */
 
 /*
-  Add written explanation of your solution here
+The hasTargetSum function creates a Set called complements to store 
+the complement of each element in the array. It then iterates over each element in the array, 
+checking if the complement of the current element is in the Set. If it is, the function returns 
+true because we have found a pair of numbers that add up to the target. If the complement is not 
+in the Set, the function adds the current element's complement to the Set. If we iterate over the entire
+ array without finding a pair that adds up to the target, the function returns false.
 */
 
 // You can run `node index.js` to view these console logs
-if (require.main === module) {
+  if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
@@ -29,6 +57,6 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
-}
+  }
 
-module.exports = hasTargetSum;
+  module.exports = hasTargetSum;
